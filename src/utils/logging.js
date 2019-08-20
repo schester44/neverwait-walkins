8f9a63e5da0fc385092e20f1df503f43db3027e4
Steven Chester <steven.chester@industryweapon.com>
2019-08-20 16:24:45 -0400

    use sentry in prod only

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -3,17 +3,19 @@ import * as Sentry from '@sentry/browser'
 
 const isProd = process.env.NODE_ENV === 'production'
 
-Sentry.init({
-	environment: process.env.NODE_ENV,
-	dsn: process.env.REACT_APP_SENTRY_DSN,
-	integrations(integrations) {
-		if (!isProd) {
-			return integrations.filter(integration => integration.name !== 'Breadcrumbs')
-		}
+if (isProd) {
+	Sentry.init({
+		environment: process.env.NODE_ENV,
+		dsn: process.env.REACT_APP_SENTRY_DSN,
+		integrations(integrations) {
+			if (!isProd) {
+				return integrations.filter(integration => integration.name !== 'Breadcrumbs')
+			}
 
-		return integrations
-	}
-})
+			return integrations
+		}
+	})
+}
 
 Sentry.configureScope(scope => {
 	const storedToken = localStorage.getItem('AuthToken')
